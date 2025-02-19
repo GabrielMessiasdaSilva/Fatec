c494c592a40fc7a8bb0ca0173c72c2a74cbf249d
GabrielMessiasdaSilva <gabrielestudante420@gmail.com>
2025-02-19 19:29:35 -0300

    apredendo

---
+++ Estrutura-de-Dados/EdTeste.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minhaprimeiraed;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class EdTeste {
+
+    public static void main(String[] args) {
+        MinhaPrimeiraED lista = new MinhaPrimeiraED();
+
+        // Testando se a lista começa vazia
+        System.out.println("Lista está vazia? " + lista.vazio());
+
+
+        lista.adiciona("1");
+        lista.adiciona("2");
+        lista.adiciona("3");
+        exibirLista(lista, "Lista após adições:");
+
+
+        System.out.println("\nTamanho da lista: " + lista.tamanho());
+
+        
+        // Adicionando um item em uma posição específica
+        lista.adiciona(1, "Gabriel"); // Esse "Gabriel" deve entrar na posição 1 e empurrar os outros
+        exibirLista(lista, "Lista após adicionar 'Gabriel' na posição 1:");
+
+     
+        lista.remove(0);
+        exibirLista(lista, "Lista após remover o elemento da posição 0:");
+
+        // Verificando se certos elementos estão na lista
+        System.out.println("\nA lista contém '2'? " + lista.contem("2")); // Deve ser true
+        System.out.println("A lista contém '5'? " + lista.contem("5")); // Deve ser false
+        System.out.println("Objeto na posição 1: " + lista.getObjeto(1)); // Aqui deve imprimir "2"
+
+        
+        // Adicionando mais elementos pra ver se a lista cresce automaticamente
+        for (int i = 4; i <= 15; i++) {
+            lista.adiciona(i + "");
+        }
+        exibirLista(lista, "Lista após adicionar mais elementos:");
+
+       
+        System.out.println("\nA lista está cheia? " + lista.cheio()); // Deve ser false porque a lista cresce sozinha
+
+        // Removendo todos os elementos da lista
+        while (!lista.vazio()) {
+            lista.remove(0); // Vai removendo o primeiro até ficar vazia
+        }
+        exibirLista(lista, "Lista após remover todos os elementos:");
+
+    
+        System.out.println("Lista está vazia? " + lista.vazio());
+    }
+
+   
+    private static void exibirLista(MinhaPrimeiraED lista, String mensagem) {
+        System.out.println("\n" + mensagem);
+        for (int i = 0; i < lista.tamanho(); i++) {
+            System.out.println(lista.getObjeto(i));
+        }
+    }
+}
---
+++ Estrutura-de-Dados/MinhaPrimeiraED.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minhaprimeiraed;
+
+/**
+ *
+ * @author Gabriel 
+ */
+public class MinhaPrimeiraED {
+  private Object[] objetos = new Object[10];
+    public int totalDeObjetos = 0;
+
+    public int tamanho() {
+        return totalDeObjetos;
+    }
+
+    public boolean cheio() {
+        return totalDeObjetos == objetos.length;
+    }
+
+    public boolean vazio() {
+        return totalDeObjetos == 0;
+    }
+
+    private boolean posicaoOcupada(int posicao) {
+        return posicao >= 0 && posicao < totalDeObjetos;
+    }
+
+    private boolean posicaoValida(int posicao) {
+        return posicao >= 0 && posicao <= totalDeObjetos;
+    }
+
+    public void adiciona(Object objeto) {
+        if (totalDeObjetos == objetos.length) {
+            return; // Não adiciona se estiver cheio
+        }
+        this.objetos[this.totalDeObjetos] = objeto;
+        this.totalDeObjetos++;
+    }
+
+    public void adiciona(int posicao, Object objeto) {
+        if (!posicaoValida(posicao)) {
+            System.out.println("Aviso: Posição inválida!");
+        }
+
+        if (totalDeObjetos == objetos.length) {
+            garantaEspaco(); // ele só vai aumentar caso estiver cheio
+        }
+
+        for (int i = this.totalDeObjetos - 1; i >= posicao; i--) {
+            objetos[i + 1] = objetos[i]; //vai deslocar para a direita 
+        }
+
+        objetos[posicao] = objeto;
+        totalDeObjetos++;
+    }
+
+    private void garantaEspaco() {
+        if (totalDeObjetos == objetos.length) {
+            Object[] novoVetor = new Object[objetos.length * 2];
+            for (int i = 0; i < objetos.length; i++) {
+                novoVetor[i] = objetos[i];
+            }
+            objetos = novoVetor;
+        }
+    }
+
+    public void remove(int posicao) {
+        if (!this.posicaoOcupada(posicao)) {
+            System.out.println("Aviso: Posição inválida!");
+        }
+
+        for (int i = posicao; i < totalDeObjetos - 1; i++) {
+            this.objetos[i] = this.objetos[i + 1];
+        }
+
+        this.objetos[totalDeObjetos - 1] = null;
+        this.totalDeObjetos--;
+    }
+
+    public boolean contem(Object objeto) {
+        for (int i = 0; i < totalDeObjetos; i++) {
+            if (objeto.equals(this.objetos[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Object getObjeto(int posicao) {
+        if (!posicaoOcupada(posicao)) {
+            System.out.println("Aviso: Posição inválida!");
+        }
+        return objetos[posicao];
+    }
+
+}
